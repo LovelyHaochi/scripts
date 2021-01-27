e2fe6f67257bd9f208648a310599b7ff0d42787b
LovelyHaochi <chenge912@qq.com>
2021-01-27 16:34:02 +0800

    修复了夏季二魔改面板导致无法获取订阅的bug

--- gobestway.py
+++ gobestway.py
@@ -15,7 +15,7 @@ while True:
     print("Count: %s" % count)
 
     # register #
-    url = 'http://find.gobestway.org/auth/register'
+    url = 'https://find.gobestway.org/auth/register'
     em = ''.join(random.sample(string.ascii_letters + string.digits, 16))
     email = em + '@gmail.com'
     print('Email: %s' % email)
@@ -32,7 +32,7 @@ while True:
     print(json.loads(res.text)['msg'])
 
     # login #
-    url = 'http://find.gobestway.org/auth/login'
+    url = 'https://find.gobestway.org/auth/login'
     data = {
         'email': email,
         'passwd': 'sbsbsbsb123',
@@ -43,20 +43,20 @@ while True:
     cookies = res.cookies
 
     # check-in #
-    # url = 'http://find.gobestway.org/user/checkin'
+    # url = 'https://find.gobestway.org/user/checkin'
     # res = requests.post(url, {}, proxies=proxies, cookies=cookies)
     # print(json.loads(res.text)['msg'])
 
     # get links #
-    url = 'http://find.gobestway.org/user'
+    url = 'https://find.gobestway.org/user'
     res = requests.get(url, proxies=proxies, cookies=cookies)
     html = etree.HTML(res.text)
-    link = html.xpath('//button/@data-clipboard-text')
-    print(link[1])
+    link = 'https://find.gobestway.org/link/' + ((html.xpath("//a[@class='btn-dl']/@href"))[1].replace("clash://install-config?url=https%3A%2F%2Ffind.gobestway.org%2Flink%2F", "")).replace("%3Fclash%3D1", "") + '?sub=4'
+    print(link)
 
     # output #
     with open('gobestway.txt', 'a+', encoding='utf-8')as f:
-        f.write(link[1])
+        f.write(link)
 
     # 计数哒 #
     count += 1
