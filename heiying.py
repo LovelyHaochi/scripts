1514ece538c11ecf1bc832c92c96a8000c42a985
LovelyHaochi <chenge912@qq.com>
2021-02-12 13:53:48 +0800

    爷改了亿下 不然屑语录获取⑧完整

--- heiying.py
+++ heiying.py
@@ -1,13 +1,16 @@
 import json
 import requests
 
-text = requests.get("https://tg.i-c-a.su/json/heiyingshabi?limit=1000").text
-text = json.loads(text)
 temp = []
-for msg in range(len(text["messages"])):
-    temp.append(text["messages"][msg]["message"])
+for count in range(2):
+    print(count+1)
+    text = json.loads(requests.get("https://tg.i-c-a.su/json/heiyingshabi/"+str(count+1)+"?limit=1000").text)
+    for msg in range(len(text["messages"])):
+        temp.append(text["messages"][msg]["message"])
+
 temp = [x for x in temp if x != '']
 yulu = []
 for string in temp:
     yulu.append(string.replace("<br />", ""))
+
 print(json.dumps(yulu, ensure_ascii=False))
