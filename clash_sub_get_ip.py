9e41666df6d7cc3b761a2fb1b8faa2dd2590ef8b
LovelyHaochi <chenge912@qq.com>
2021-02-20 06:16:11 -0800

    新增查重

--- clash_sub_get_ip.py
+++ clash_sub_get_ip.py
@@ -4,8 +4,10 @@
 import yaml
 import requests
 import socket
+import sys
 
-clash_sub_link = input("Clash Sub Link >> ")
+# clash_sub_link = input("Clash Sub Link >> ")
+clash_sub_link = sys.argv[1]
 proxies = {
     'http': 'http://localhost:7890',
     'https': 'http://localhost:7890',
@@ -22,24 +24,43 @@ def get_ip(domain):
         return '{} ({})'.format(error, domain)
 
 
-y = yaml.load((requests.get(clash_sub_link, proxies=proxies, headers=headers)).text, Loader=yaml.FullLoader)
-print(y)
+def main():
+    y = yaml.load((requests.get(clash_sub_link, proxies=proxies, headers=headers)).text, Loader=yaml.FullLoader)
+    # y = yaml.load((requests.get(clash_sub_link, headers=headers)).text, Loader=yaml.FullLoader)
+    # print(y)
 
-print('')
+    ip = []
 
-print('--- Domain list ---')
-if y['proxies']:
-    for num in y['proxies']:
-        print(num['server'])
-else:
-    for num in y['Proxy']:
-        print(num['server'])
-print('')
+    if y['proxies']:
+        for num in y['proxies']:
+            ip.append(num['server'])
+    else:
+        for num in y['Proxy']:
+            ip.append(num['server'])
 
-print('--- IP list ---')
-if y['proxies']:
-    for num in y['proxies']:
-        print(get_ip(num['server']))
-else:
-    for num in y['Proxy']:
-        print(get_ip(num['server']))
+    print('')
+
+    print('--- Domain list ---')
+    for num in ip:
+        print(num)
+    print('')
+
+    print('--- IP list ---')
+    for num in ip:
+        print(get_ip(num))
+    print('')
+
+    repeat = {}
+    print('--- Repeat list ---')
+    list = ip
+    lists = set(list)
+    count = 0
+    for item in lists:
+        repeat[item] = list.count(item)
+        count += list.count(item)
+    repeat = sorted(repeat.items(), key=lambda item: item[1], reverse=True)
+    for item in repeat:
+        print("{}: {}".format(item[0], str(item[1] / count * 100)[:4] + '%'))
+
+
+main()
