8e42e1a3a28b4ecb9472310b426f57b0458cf027
LovelyHaochi <49602236+LovelyHaochi@users.noreply.github.com>
2021-03-02 07:34:06 -0800

    Create galaxy-renew.py

---
+++ galaxy-renew.py
@@ -0,0 +1,46 @@
+# By LovelyHaochi
+# link: https://github.com/LovelyHaochi/
+
+import requests
+
+username = ''
+passwd = ''
+proxies = {
+    'http': 'http://localhost:7890',
+    'https': 'http://localhost:7890',
+}
+useProxy = False
+
+
+proxies = proxies if useProxy else {}
+
+if __name__ == '__main__':
+    print("--- 开始登录 ---")
+    url = 'https://galaxy-cloud.link/api/v1/passport/auth/login'
+    data = {
+        "email": username,
+        "password": passwd
+    }
+    res = requests.post(url, data, proxies=proxies)
+    cookies = res.cookies
+
+    print("--- 开始提交save ---")
+    url = 'https://galaxy-cloud.link/api/v1/user/order/save'
+    data = {
+        "cycle": "onetime_price",
+        "plan_id": "1"
+    }
+    res = requests.post(url, data, proxies=proxies, cookies=cookies).json()
+    print(res)
+    id = res['data']
+
+    print("--- 开始Checkout ---")
+    print("订单号: {}".format(id))
+    url = 'https://galaxy-cloud.link/api/v1/user/order/checkout'
+    data = {
+        "trade_no": id,
+        "method": "4"
+    }
+    res = requests.post(url, data, proxies=proxies, cookies=cookies).json()
+    print(res['data'])
+
